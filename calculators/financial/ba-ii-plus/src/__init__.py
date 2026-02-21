77a4b3d8897f306731ad1fdfb133792c5121731c
Shankar Venkatraman <shankar.venkatraman15@gmail.com>
2026-02-21 16:41:42 +0000

    Fix __init__.py: import FinancialCalculator not Calculator

--- calculators/financial/ba-ii-plus/src/__init__.py
+++ calculators/financial/ba-ii-plus/src/__init__.py
@@ -4,7 +4,7 @@ A comprehensive implementation of the Texas Instruments BA II Plus calculator
 with support for TVM calculations, statistics, cash flow analysis, and financial metrics.
 """
 
-from .calculator import Calculator
+from .calculator import FinancialCalculator
 
 __version__ = "1.0.0"
-__all__ = ["Calculator"]
+__all__ = ["FinancialCalculator"]
